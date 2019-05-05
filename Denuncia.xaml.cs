3906a7166311555f2a19aed9a19949d40aa3775b
unknown <kennedy_snp@hotmail.com>
2019-05-04 21:56:03 -0400

    first commit

---
+++ Denuncia.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Denuncie
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class Denuncia : ContentPage
+	{
+		public Denuncia ()
+		{
+			InitializeComponent ();
+		}
+
+        private async void BtImageLocation_Clicked(object sender, EventArgs e)
+        {
+            await DisplayAlert("Mensagem!", "Localização obtida com sucesso!", "OK");
+        }
+
+        private async void BtEscolherImagem_Clicked(object sender, EventArgs e)
+        {
+            string action = await DisplayActionSheet("Anexar uma imagem!", "Cancel", null, "Tirar foto", "Imagem da Galeria");
+        }
+
+        private async void BtEnviar_Clicked(object sender, EventArgs e)
+        {
+            await DisplayAlert("Mensagem!", "Denúncia enviada com sucesso!", "OK");
+            await Navigation.PopToRootAsync();
+        }
+    }
+}
---
+++ Emergencia.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Denuncie
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class Emergencia : ContentPage
+	{
+		public Emergencia ()
+		{
+			InitializeComponent ();
+		}
+
+        private async void BtdenunciaDetalhada_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new Denuncia());
+        }
+
+        private async void ImageCircle_Clicked(object sender, EventArgs e)
+        {
+            bool answer = await DisplayAlert("Mensagem!", "Tem certeza que deseja enviar? \n\nObs: Sua localização será enviada!", "Sim", "Não");
+
+            if (answer == true)
+            {
+                await DisplayAlert("Mensagem!", "Pedido enviado com sucesso!", "OK");
+                await Navigation.PopToRootAsync();
+            }
+        }
+    }
+}
